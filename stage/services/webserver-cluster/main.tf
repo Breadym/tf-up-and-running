a8c68160c7ebe3e05586b97411728638c7f11a06
Bready <joshbreadmore@gmail.com>
2026-09-04 11:38:01 +1200

    bunch of random chnages that have broken build

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,17 +1,16 @@
-provider "aws" {
-  region = "ap-southeast-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
 }
 
-data "aws_availability_zones" "all" {}
-
-data "aws_remote_state" "db" {
-  backend = "s3" 
-  
-  config {
-    bucket  = "a-really-unique-name-that-no-one-else-has-used-2"
-    key     = "global/s3/terraform.tfstate"
-    region  = "ap-southeast-2"
-  }
+provider "aws" {
+  region = "ap-southeast-2"
 }
 
 resource "aws_launch_template" "example" {
@@ -19,27 +18,11 @@ resource "aws_launch_template" "example" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = templatefile("user-data.sh",
-    {
+  user_data = templatefile("user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
   })
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = "tf-sg-instance"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   lifecycle {
     create_before_destroy = true
@@ -64,6 +47,21 @@ resource "aws_autoscaling_group" "example" {
     key                 = "Name"
     value               = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = var.instance_security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -107,5 +105,29 @@ resource "aws_security_group" "elb" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+
+data "aws_availability_zones" "all" {}
+
+data "terraform_remote_state" "db" {
+  backend = "s3" 
+  
+  config = {
+    bucket  = var.db_remote_state_bucket # "a-really-unique-name-that-no-one-else-has-used-2"
+    key     = var.db_remote_state_key # "global/s3/terraform.tfstate"
+    region  = "ap-southeast-2"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
--- stage/services/webserver-cluster/var.tf
+++ stage/services/webserver-cluster/var.tf
@@ -2,11 +2,13 @@
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket used for the database's remote state storage"
   type        = string
+  default     = "a-really-unique-name-that-no-one-else-has-used-2"
 }
 
 variable "db_remote_state_key" {
   description = "The name of the key in the S3 bucket used for the database's remote state storage"
   type        = string
+  default     = "global/s3/terraform.tfstate"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
